7f1430cb4c44e9fc75fcf1d2f823a7be88f000c1
Hanna K <anna_kon@bigmir.net>
2024-10-13 21:58:03 +0300

    add new company page, intercepting routes example

--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 
 import { Status } from '@/app/components/status-label';
-import Header from '@/app/components/header';
-import Toolbar from '@/app/components/toolbar';
-import SearchInput from '@/app/components/search-input';
+// import Header from '@/app/components/header';
+// import Toolbar from '@/app/components/toolbar';
+// import SearchInput from '@/app/components/search-input';
 import CompanyTable from '@/app/components/company-table';
 import CompanyRow from '@/app/components/company-row';
-import AddCompanyButton from '@/app/components/add-company-button';
-
+// import AddCompanyButton from '@/app/components/add-company-button';
 
 type Props = {};
 
 const Page = (props: Props) => {
   return (
     <>
-      <Header>Companies</Header>
+      {/* <Header>Companies</Header>
       <Toolbar action={<AddCompanyButton />}>
         <SearchInput />
-      </Toolbar>
+      </Toolbar> */}
       {/* використовуємо готоу кнопку, яка викликає форму для створення компанії */}
       <CompanyTable>
         <CompanyRow
--- src/app/components/add-company-button.tsx
+++ src/app/components/add-company-button.tsx
@@ -1,24 +1,37 @@
-'use client';
+// 'use client';
 
-import React, { useState } from 'react';
-import dynamic from 'next/dynamic';
-import Button from '@/app/components/button';
+// import React, { useState } from 'react';
+// import dynamic from 'next/dynamic';
+// import Button from '@/app/components/button';
 // import CompanyFormModal from '@/app/components/company-form-modal';
 
-const CompanyFormModal = dynamic(() => import('./company-form-modal'), {
-  ssr: false,
-});
+// const CompanyFormModal = dynamic(() => import('./company-form-modal'), {
+//   ssr: false,
+// });
+
+// export default function AddCompanyButton() {
+//   const [show, setShow] = useState(false);
+//   return (
+//     <>
+//       <Button onClick={() => setShow(true)}>Add company</Button>
+//       <CompanyFormModal
+//         onSubmit={console.log}
+//         show={show}
+//         onClose={() => setShow(false)}
+//       />
+//     </>
+//   );
+// }
+
+'use client';
+
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import Button from '@/app/components/button';
 
 export default function AddCompanyButton() {
-  const [show, setShow] = useState(false);
+  const router = useRouter();
   return (
-    <>
-      <Button onClick={() => setShow(true)}>Add company</Button>
-      <CompanyFormModal
-        onSubmit={console.log}
-        show={show}
-        onClose={() => setShow(false)}
-      />
-    </>
+    <Button onClick={() => router.push('/companies/new')}>Add company</Button>
   );
 }
