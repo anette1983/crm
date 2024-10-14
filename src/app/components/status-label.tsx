ac4ce5ab56d1b0e2e4e528b06cff928e06a3b42d
Hanna K <anna_kon@bigmir.net>
2024-10-14 16:50:48 +0300

    add react query changes to files

--- src/app/components/status-label.tsx
+++ src/app/components/status-label.tsx
@@ -1,28 +1,39 @@
 import React from 'react';
 import clsx from 'clsx';
 
-export enum Status {
-  Active = 'active',
-  NotActive = 'notActive',
-  Pending = 'pending',
-  Suspended = 'suspended',
-}
+// export enum Status {
+//   Active = 'active',
+//   NotActive = 'notActive',
+//   Pending = 'pending',
+//   Suspended = 'suspended',
+// }
+
+import { CompanyStatus } from '@/lib/api';
 
 export interface StatusLabelProps {
-  children: React.ReactNode;
-  status: Status;
+  // children: React.ReactNode;
+  // status: Status;
+  status: CompanyStatus;
   disabled?: boolean;
 }
 
-function StatusLabel({ children, status, disabled }: StatusLabelProps) {
-  // console.log('env in status label', process.env.CRM_SECRET_KEY);
-  // Всі компоненти за замовчуванням серверні
-  // Під час рендерингу тобто можемо отримати доступ до ресурсів серверу
-  // Проте з клієнта – бразуера – ці ресурси не будуть доступні
-  // змінна доступна лише в консолі, не в консолі браузера
-  // створили: export CRM_SECRET_KEY=123
-  // потім перезапустили проект
+// function StatusLabel({ children, status, disabled }: StatusLabelProps) {
+// console.log('env in status label', process.env.CRM_SECRET_KEY);
+// Всі компоненти за замовчуванням серверні
+// Під час рендерингу тобто можемо отримати доступ до ресурсів серверу
+// Проте з клієнта – бразуера – ці ресурси не будуть доступні
+// змінна доступна лише в консолі, не в консолі браузера
+// створили: export CRM_SECRET_KEY=123
+// потім перезапустили проект
 
+const labelByStatus = {
+  [CompanyStatus.Active]: 'Active',
+  [CompanyStatus.NotActive]: 'Not Active',
+  [CompanyStatus.Pending]: 'Pending',
+  [CompanyStatus.Suspended]: 'Suspended',
+};
+
+export default function StatusLabel({ status, disabled }: StatusLabelProps) {
   return (
     <div
       // className={clsx(
@@ -36,10 +47,14 @@ function StatusLabel({ children, status, disabled }: StatusLabelProps) {
       // )}
       className={clsx(
         'inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium',
-        status === Status.Active && 'text-green-700 bg-green-100',
-        status === Status.NotActive && 'text-red-700 bg-red-100',
-        status === Status.Pending && 'text-orange-700 bg-orange-100',
-        status === Status.Suspended && 'text-blue-700 bg-blue-100',
+        // status === Status.Active && 'text-green-700 bg-green-100',
+        // status === Status.NotActive && 'text-red-700 bg-red-100',
+        // status === Status.Pending && 'text-orange-700 bg-orange-100',
+        // status === Status.Suspended && 'text-blue-700 bg-blue-100',
+        status === CompanyStatus.Active && 'text-green-700 bg-green-100',
+        status === CompanyStatus.NotActive && 'text-red-700 bg-red-100',
+        status === CompanyStatus.Pending && 'text-orange-700 bg-orange-100',
+        status === CompanyStatus.Suspended && 'text-blue-700 bg-blue-100',
         '',
         {
           ['opacity-75 cursor-not-allowed']: disabled,
@@ -48,12 +63,11 @@ function StatusLabel({ children, status, disabled }: StatusLabelProps) {
     >
       {/* bullet */}
       <div className="w-1 h-1 mr-2 rounded-full bg-current" />
-      {children}
+      {/* {children} */}
+      {labelByStatus[status]}
     </div>
   );
 }
-
-export default StatusLabel;
 
 /*clsx принимает любое количество аргументов, которые могут быть строками, объектами или массивами, и возвращает одну строку с классами, игнорируя все ложные значения (такие как false, null, undefined и пустые строки).
 
